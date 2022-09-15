b5a52bc7e767a814c37f2a6f7f652603484bea54
Lorn Samnang <samnanglorn@gmail.com>
2022-09-15 20:24:26 +0700

    Config Admin Template (Admin LTE)

--- src/main.js
+++ src/main.js
@@ -5,6 +5,16 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+
+import 'admin-lte/plugins/fontawesome-free/css/all.min.css'
+import 'admin-lte/dist/css/adminlte.min.css?v=3.2.0'
+import './style.scss'
+
+import 'admin-lte/plugins/jquery/jquery.min.js'
+import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js'
+import 'admin-lte/dist/js/adminlte.min.js?v=3.2.0'
+
+
 new Vue({
   router,
   store,
